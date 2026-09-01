d5ada5843e815ba66a37a726509b92ba5768c786
flexo <akiyaono2000@gmail.com>
2026-09-01 01:51:01 -0500

    Add the generic celestial body definition

---
+++ Assets/CelestialSystems/Runtime/CelestialBodyDefinition.cs
@@ -0,0 +1,115 @@
+/*
+ * Stores reusable physical, generation, and surface-system settings for a celestial body.
+ */
+
+using UnityEngine;
+
+namespace jcan.CelestialSystems
+{
+    [CreateAssetMenu(
+        fileName = "Celestial Body",
+        menuName = "Celestial Systems/Celestial Body")]
+    public sealed class CelestialBodyDefinition :
+        ScriptableObject
+    {
+        public const double DefaultRoundBodyMinimumDiameterMeters =
+            1000000.0;
+
+        [SerializeField]
+        private string definitionId =
+            "body";
+
+        [SerializeField]
+        private double massKilograms =
+            1.0;
+
+        [SerializeField]
+        private double referenceRadiusMeters =
+            500000.0;
+
+        [SerializeField]
+        private int generationSeed;
+
+        [SerializeField]
+        private CelestialSurfaceSystem surfaceSystem =
+            CelestialSurfaceSystem.AutomaticByDiameter;
+
+        [SerializeField]
+        private RoundMapMagicSurfaceDefinition roundMapMagicSurface;
+
+        public string DefinitionId =>
+            definitionId;
+
+        public double MassKilograms =>
+            massKilograms;
+
+        public double ReferenceRadiusMeters =>
+            referenceRadiusMeters;
+
+        public double DiameterMeters =>
+            referenceRadiusMeters *
+            2.0;
+
+        public int GenerationSeed =>
+            generationSeed;
+
+        public CelestialSurfaceSystem SurfaceSystem =>
+            surfaceSystem;
+
+        public CelestialSurfaceSystem ResolvedSurfaceSystem
+        {
+            get
+            {
+                if (surfaceSystem !=
+                    CelestialSurfaceSystem.AutomaticByDiameter)
+                {
+                    return surfaceSystem;
+                }
+
+                return DiameterMeters >=
+                    DefaultRoundBodyMinimumDiameterMeters
+                        ? CelestialSurfaceSystem.RoundMapMagic
+                        : CelestialSurfaceSystem.Irregular;
+            }
+        }
+
+        public RoundMapMagicSurfaceDefinition RoundMapMagicSurface =>
+            roundMapMagicSurface;
+
+        public bool HasValidPhysicalSettings =>
+            !string.IsNullOrWhiteSpace(
+                definitionId) &&
+            IsFinite(massKilograms) &&
+            massKilograms > 0.0 &&
+            IsFinite(referenceRadiusMeters) &&
+            referenceRadiusMeters > 0.0;
+
+        public bool HasValidResolvedSurfaceSettings
+        {
+            get
+            {
+                switch (ResolvedSurfaceSystem)
+                {
+                    case CelestialSurfaceSystem.RoundMapMagic:
+                        return
+                            roundMapMagicSurface != null &&
+                            roundMapMagicSurface.HasValidSettings;
+
+                    case CelestialSurfaceSystem.Irregular:
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        }
+
+        private static bool IsFinite(
+            double value)
+        {
+            return
+                !double.IsNaN(value) &&
+                !double.IsInfinity(value);
+        }
+    }
+}
