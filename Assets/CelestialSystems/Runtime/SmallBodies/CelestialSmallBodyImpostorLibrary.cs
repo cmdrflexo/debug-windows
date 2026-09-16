b8f62f8694ce3511dca13908129a44866b9d39a1
flexo <akiyaono2000@gmail.com>
2026-09-16 16:18:08 -0500

    Allocate requested small-body impostor atlas channels

--- Assets/CelestialSystems/Runtime/SmallBodies/CelestialSmallBodyImpostorLibrary.cs
+++ Assets/CelestialSystems/Runtime/SmallBodies/CelestialSmallBodyImpostorLibrary.cs
@@ -32,6 +32,18 @@ namespace jcan.CelestialSystems
         [SerializeField]
         private bool isConfigured;
 
+        [SerializeField]
+        private RenderTexture albedoTransparencyAtlas;
+
+        [SerializeField]
+        private RenderTexture normalAtlas;
+
+        [SerializeField]
+        private RenderTexture emissionAtlas;
+
+        [SerializeField]
+        private RenderTexture metallicSmoothnessAtlas;
+
         public string ToolId =>
             toolId;
 
@@ -46,6 +58,18 @@ namespace jcan.CelestialSystems
 
         public bool IsConfigured =>
             isConfigured;
+
+        public RenderTexture AlbedoTransparencyAtlas =>
+            albedoTransparencyAtlas;
+
+        public RenderTexture NormalAtlas =>
+            normalAtlas;
+
+        public RenderTexture EmissionAtlas =>
+            emissionAtlas;
+
+        public RenderTexture MetallicSmoothnessAtlas =>
+            metallicSmoothnessAtlas;
 
         public void Configure(
             string newToolId,
@@ -74,7 +98,126 @@ namespace jcan.CelestialSystems
                 Mathf.CeilToInt(
                     variantCount /
                     (float)atlasColumnCount);
+            AllocateRequestedAtlases();
             isConfigured = true;
+        }
+
+        private void OnDestroy()
+        {
+            ReleaseAtlas(
+                ref albedoTransparencyAtlas);
+            ReleaseAtlas(
+                ref normalAtlas);
+            ReleaseAtlas(
+                ref emissionAtlas);
+            ReleaseAtlas(
+                ref metallicSmoothnessAtlas);
+        }
+
+        private void AllocateRequestedAtlases()
+        {
+            var atlasWidth =
+                atlasColumnCount *
+                variantResolution;
+            var atlasHeight =
+                atlasRowCount *
+                variantResolution;
+
+            AllocateAtlas(
+                ref albedoTransparencyAtlas,
+                CelestialSmallBodyImpostorMaps
+                    .AlbedoTransparency,
+                "Albedo Transparency",
+                RenderTextureFormat.ARGB32,
+                atlasWidth,
+                atlasHeight);
+            AllocateAtlas(
+                ref normalAtlas,
+                CelestialSmallBodyImpostorMaps.Normal,
+                "Normal",
+                RenderTextureFormat.ARGB32,
+                atlasWidth,
+                atlasHeight);
+            AllocateAtlas(
+                ref emissionAtlas,
+                CelestialSmallBodyImpostorMaps.Emission,
+                "Emission",
+                RenderTextureFormat.ARGBHalf,
+                atlasWidth,
+                atlasHeight);
+            AllocateAtlas(
+                ref metallicSmoothnessAtlas,
+                CelestialSmallBodyImpostorMaps
+                    .MetallicSmoothness,
+                "Metallic Smoothness",
+                RenderTextureFormat.ARGB32,
+                atlasWidth,
+                atlasHeight);
+        }
+
+        private void AllocateAtlas(
+            ref RenderTexture atlas,
+            CelestialSmallBodyImpostorMaps map,
+            string mapName,
+            RenderTextureFormat format,
+            int width,
+            int height)
+        {
+            if ((requestedMaps & map) == 0)
+            {
+                ReleaseAtlas(
+                    ref atlas);
+                return;
+            }
+
+            if (atlas != null &&
+                atlas.width == width &&
+                atlas.height == height &&
+                atlas.format == format)
+            {
+                return;
+            }
+
+            ReleaseAtlas(
+                ref atlas);
+            atlas =
+                new RenderTexture(
+                    width,
+                    height,
+                    0,
+                    format,
+                    RenderTextureReadWrite.Linear)
+                {
+                    name =
+                        $"{toolId} Impostor {mapName} Atlas",
+                    useMipMap = true,
+                    autoGenerateMips = true
+                };
+            atlas.Create();
+        }
+
+        private static void ReleaseAtlas(
+            ref RenderTexture atlas)
+        {
+            if (atlas == null)
+            {
+                return;
+            }
+
+            atlas.Release();
+
+            if (Application.isPlaying)
+            {
+                Destroy(
+                    atlas);
+            }
+            else
+            {
+                DestroyImmediate(
+                    atlas);
+            }
+
+            atlas = null;
         }
 
         public int GetVariantIndex(
